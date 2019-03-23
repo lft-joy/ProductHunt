f0efdccd0b59257f69f8f9baf4eb029db36fa234
lft <875509000@qq.com>
2019-03-23 17:07:06 +0800

    完成了注册页面
    实现了注册页面，构建了简单的表单验证，实现注册页面与主页的交互。

--- account/views.py
+++ account/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    elif request.method == 'POST':
+        user_name = request.POST['user']
+        pass_word1 = request.POST['word1']
+        pass_word2 = request.POST['word2']
+        try:
+            User.objects.get(username=user_name)
+            return render(request, 'signup.html', {'用户名错误': '该用户已存在'})
+        except User.DoesNotExist:
+            if pass_word1 == pass_word2:
+                User.objects.create(username=user_name, password=pass_word1)
+                return redirect('主页')
+            else:
+                return render(request, 'signup.html', {'密码错误': '两次密码不一致'})
