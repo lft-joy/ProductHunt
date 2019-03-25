000738399025d6e8e3c296a5ad9d83f168d57b7e
lft <875509000@qq.com>
2019-03-25 10:23:59 +0800

    完善了发布页面
    能够将发布页面的数据传送到数据库中,能在数据库中对数据进行编辑.(开始有个小bug是我没有将数据进行迁移,导致一直找不到数据,所以现在每一次对model里面的数据进行修改都需要进行数据迁移)

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(default='例:app名称', max_length=50)
+    intro = models.TextField(default='app介绍')
+    icon = models.ImageField(default='default.png', upload_to='image/')
+
+    votes = models.IntegerField(default=1)
+    pub_date = models.DateTimeField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 
 # Create your views here.
@@ -9,4 +12,20 @@ def products_list(requests):
 
 
 def product(request):
-    return render(request, 'product.html')
+    if request.method == 'GET':
+        return render(request, 'product.html')
+    elif request.method == 'POST':
+        title = request.POST['APP名称']
+        intro = request.POST['介绍']
+        try:
+            icon = request.POST['图片']
+            product = Product()
+            product.title = title
+            product.icon = icon
+            product.intro = intro
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('主页')
+        except Exception as e:
+            return render(request, 'products_list.html', {'错误': '请上传图片'})
